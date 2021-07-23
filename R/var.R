5618ed7dacd169d93ee7280c37ddfb8cb1a444df
nanabi <nguyenngocbinhneu@gmail.com>
2021-07-23 23:59:20 +0700

    add var model

--- R/var.R
+++ R/var.R
@@ -1,26 +1,31 @@
-set.seed(123) # Reset random number generator for reasons of reproducability
-
-# Generate sample
-t <- 200 # Number of time series observations
-k <- 2 # Number of endogenous variables
-p <- 2 # Number of lags
-
-# Generate coefficient matrices
-A.1 <- matrix(c(-.3, .6, -.4, .5), k) # Coefficient matrix of lag 1
-A.2 <- matrix(c(-.1, -.2, .1, .05), k) # Coefficient matrix of lag 2
-A <- cbind(A.1, A.2) # Companion form of the coefficient matrices
-
-# Generate series
-series <- matrix(0, k, t + 2*p) # Raw series with zeros
-for (i in (p + 1):(t + 2*p)){ # Generate series with e ~ N(0,0.5)
-  series[, i] <- A.1%*%series[, i-1] + A.2%*%series[, i-2] + rnorm(k, 0, .5)
-}
-
-series <- ts(t(series[, -(1:p)])) # Convert to time series format
-names <- c("V1", "V2") # Rename variables
-
-plot.ts(series) # Plot the series
-
+library(dplyr)
+library(lubridate)
 library(vars) # Load package
+library(xts)
 
-var.1 <- VAR(series, 2, type = "none") # Estimate the model
+ticker_name <- "fpt"
+
+df <- readr::read_csv(paste0("data/excel_", ticker_name , ".csv")) %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(date = ymd(dtyyyymmdd),
+         value = close_fixed) %>%
+  dplyr::arrange(date) %>% 
+  mutate(lag_vol = lag(volume)) %>% 
+  dplyr::select(date, open_fixed,  high_fixed,  low_fixed, close_fixed, volume, lag_vol) %>% 
+  na.omit()
+
+
+series <- xts(x = dplyr::select(df, open_fixed,  high_fixed,  low_fixed, close_fixed, volume), order.by = df$date)
+# series <- xts(x = dplyr::select(df, close_fixed, volume), order.by = df$date)
+exogen_series <- xts(x = dplyr::select(df, volume), order.by = df$date)
+
+# var.1 <- VAR(series, 2, type = "none") # Estimate the model
+var.aic <- VAR(series, type = "none", lag.max = 3, ic = "AIC")
+summary(var.aic)
+
+# Extract coefficients, standard errors etc. from the object
+# produced by the VAR function
+est_coefs <- coef(var.aic)
+
+pred <- predict(var.aic, n.ahead = 8, ci = 0.95) 
+pred$fcst$close_fixed
